d8247bfd58eb553177e7bb4872f8c5c63f1e41a3
Trent Mitchell-Borley <trent@trents.computer>
2023-02-24 21:43:09 +1300

    Add Icons

---
+++ src/StopIcon.tsx
@@ -0,0 +1,23 @@
+import { splitProps } from "solid-js";
+import { IconProps } from "./types";
+
+export default function StopIcon(props: IconProps) {
+  const [, rest] = splitProps(props, ["color"]);
+  return (
+    <svg
+      width="15"
+      height="15"
+      viewBox="0 0 15 15"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...rest}
+    >
+      <path
+        d="M2 3C2 2.44772 2.44772 2 3 2H12C12.5523 2 13 2.44772 13 3V12C13 12.5523 12.5523 13 12 13H3C2.44772 13 2 12.5523 2 12V3ZM12 3H3V12H12V3Z"
+        fill={props.color || "currentColor"}
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+      ></path>
+    </svg>
+  );
+}
